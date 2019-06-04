ef352d5bc1a04c21d4f362fe3b7a3d6b58a44d1c
86159 <19930408>
2019-06-04 09:36:41 +0800

    提交代码2

---
+++ data-supplement-common/src/main/java/com/pingan/pare/bi/common/util/DateUtil.java
@@ -0,0 +1,4 @@
+package com.pingan.pare.bi.common.util;
+
+public class DateUtil {
+}
---
+++ data-supplement-common/src/main/java/com/pingan/pare/bi/common/util/JsonUtil.java
@@ -0,0 +1,14 @@
+package com.pingan.pare.bi.common.util;
+
+import java.util.Map;
+
+public class JsonUtil {
+
+    /**
+     * json convert map
+     * @return
+     */
+    public Map<String,Object> jsonToMap(){
+        return null;
+    }
+}
